71ae82fa9ce83b4836dbc84488729f2aae3c4151
Yesil <yesil@gmail.com>
2023-11-15 17:40:59 +0300

    feat: signals and istek atma

--- profiles/models.py
+++ profiles/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from .utils import get_random_code
 from django.template.defaultfilters import slugify
+
+
 # Create your models here.
 class Profile(models.Model):
     first_name = models.CharField(max_length=100, blank=True)
@@ -15,17 +17,36 @@ class Profile(models.Model):
     slug = models.SlugField(unique=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
-        return self.first_name
-    def save(self,*args,**kwargs):
-        ex=False
+        return f"{self.user.username}-{self.created.strftime('%d-%m-%Y)}')}"
+
+    def save(self, *args, **kwargs):
+        ex = False
         if self.first_name and self.last_name:
-            to_slug=slugify(str(self.first_name)+" "+str(self.last_name))
-            ex=Profile.objects.filter(slug=to_slug).exists()
+            to_slug = slugify(str(self.first_name) + " " + str(self.last_name))
+            ex = Profile.objects.filter(slug=to_slug).exists()
             while ex:
-                to_slug=slugify(to_slug+" "+str(get_random_code()))
-                ex=Profile.objects.filter(slug=to_slug).exists()
+                to_slug = slugify(to_slug + " " + str(get_random_code()))
+                ex = Profile.objects.filter(slug=to_slug).exists()
         else:
-            to_slug=str(self.user)
-        self.slug=to_slug
-        super().save(*args,**kwargs)
+            to_slug = str(self.user)
+        self.slug = to_slug
+        super().save(*args, **kwargs)
+
+
+STATUS_CHOICES = (
+    ('send', 'send'),
+    ('accepted', 'accepted')
+)
+
+
+class Relationship(models.Model):
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
+    revicer = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='reciver')
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.sender}-{self.revicer}-{self.status}'
